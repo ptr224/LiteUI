83425bdf8dd18951b5199d9445c90e11ecf567b2
Pietro Gagliardi <pietro.gagliardi@outlook.com>
2021-12-20 19:45:41 +0100

    Fix minori colorazione

--- LiteUI.Test/MainWindow.xaml.cs
+++ LiteUI.Test/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LiteUI.Test
+﻿using System.Windows.Media;
+
+namespace LiteUI.Test
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -10,6 +12,14 @@
             InitializeComponent();
 
             DataContext = null;
+
+            SetGlobalTheme(new(
+                Color.FromRgb(0xFF, 0xFF, 0xFF),
+                Color.FromRgb(0x7F, 0x7F, 0x7F),
+                Color.FromRgb(0x27, 0x27, 0x27),
+                Color.FromRgb(0x00, 0x00, 0x00),
+                Color.FromRgb(0xFF, 0x7B, 0x00)
+            ));
         }
     }
 }
